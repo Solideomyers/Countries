9b7b51bd76bebca2822dc80b64404924cb2cd100
francisco alejandro myers muñoz <franciscomyers@gmail.com>
2023-03-28 12:01:36 -0400

    About fixed

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,14 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import Styles from "./About.module.css";
 
 const About = () => {
-    return(
-        <>
-            <Link to="/home" >
-                <h1>I'm About</h1>
-            </Link>
-        </>
-    )
+  return (
+    <div className={Styles.containerAbout}>
+      <Link to="/home" className={Styles.link}>
+        <button className={Styles.btnAbout}>Go Home</button>
+      </Link>
+      <div className={Styles.content}>
+        <h1 className={Styles.title}>I'm Francisco Myers</h1>
+        <p className={Styles.subtitle}>
+          Student of SOYHENRY, new passenger in the developer ship.
+        </p>
+      </div>
+      <div className={Styles.icons}>
+        <a
+          href="https://github.com/Solideomyers"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={Styles.iconLink}
+        >
+          <FaGithub className={Styles.icon} />
+        </a>
+        <a
+          href="https://www.linkedin.com/feed/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={Styles.iconLink}
+        >
+          <FaLinkedin className={Styles.icon} />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default About;
